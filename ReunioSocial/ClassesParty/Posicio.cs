669c7e162877dd921eb9c7f67afca4b0839b5f8b
recarens <eric.recarens@gmail.com>
2015-02-05 17:03:23 +0100

    canvi 2
    implementacio de classe posicio

---
+++ ReunioSocial/ClassesParty/Posicio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesParty
+{
+    public enum Direccio { Quiet, Amunt, Dreta, Avall, Esquerra }
+    public class Posicio
+    {
+        int columna;
+        int fila;
+
+        public int Columna
+        {
+            get { return columna; }
+            set { columna = value; }
+        }
+        public int Fila
+        {
+            get { return fila; }
+            set { fila = value; }
+        }
+        public bool Buida
+        {
+            get { return true; }
+        }
+
+        double Distancia(Posicio pos)
+        {
+            double distancia = 0;
+            int numColumnes = Math.Abs(this.Fila - pos.Fila);
+            int numFiles =  Math.Abs(this.Columna - pos.Columna);
+            distancia = Math.Sqrt((numColumnes * numColumnes) + (numColumnes * numColumnes));
+            return distancia;
+        }
+        
+    }
+}
