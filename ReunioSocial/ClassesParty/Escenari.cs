c3ba2d92c2c6985f285fe7044c9d87ff0c1ebdc6
recarens <eric.recarens@gmail.com>
2015-02-05 17:54:51 +0100

    Creació de persona i escenari

---
+++ ReunioSocial/ClassesParty/Escenari.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassesParty
+{
+    Classe Escenari
+public class Escenari
+{
+/// <summary>
+/// Crea un escenari donades unes mides
+/// </summary>
+/// <param name="files">Número de files de l'escenari</param>
+/// <param name="columnes">Número de columnes de l'escenari</param>
+public Escenari(int files, int columnes)
+/// <summary>
+/// Retorna el número de files de l'escenari
+/// </summary>
+public int Files
+{get;}
+/// <summary>
+/// Retorna el número de columnes de l'escenari
+/// </summary>
+public int Columnes
+{get ;}
+/// <summary>
+/// Retorna el número de homes que hi ha dins de l'escenari
+/// </summary>
+public int Homes
+{get ;}
+/// <summary>
+/// Retorna el número de dones que hi ha dins de l'escenari
+/// </summary>
+public int Dones
+{get ;}
+/// <summary>
+/// Retorna el número de Cambrers que hi ha dins de l'escenari
+/// </summary>
+public int Cambrers
+{get ;}
+/// <summary>
+/// Mou una persona de (filOrig, colOrig) fins a la posicio adjacent (filDesti,c
+olDesti)
+/// Es suposa que les coordenades són vàlides, que hi ha una persona a l'origen
+i que
+/// el destí està buit.
+/// </summary>
+/// <param name="filOrig">Fila de la coordenada d'origen</param>
+/// <param name="colOrig">Columna de la coordenada d'origen</param>
+/// <param name="filDesti">Fila de la coordenada de destí</param>
+/// <param name="colDesti">Columna de la coordenada de destí</param>
+private void Moure (int filOrig, int colOrig, int filDesti, int colDesti)/// <summary>
+/// Retorna la Posició que hi ha en una coordenada donada
+/// </summary>
+public Posicio this [int fila, int col]
+{
+get;
+}
+/// <summary>
+/// Mira si una coordenada es correcte per ser destí d'una persona
+/// </summary>
+/// <param name="fil">fila de la coordenada</param>
+/// <param name="col">columna de la coordenada</param>
+/// <returns>retorna si la coordenada és vàlida i està buida</returns>
+public bool DestiValid (int fil, int col)
+/// <summary>
+/// Retorna el contingut del escenari en forma de matriu d'strings,
+/// representant cada persona amb el seu nom
+/// </summary>
+/// <returns>Matriu de caràcters</returns>
+public String [,] ContingutNoms ()
+/// <summary>
+/// Elimina una persona de l'escenari i de la taula de persones
+/// </summary>
+/// <param name="fil">Fila on està la persona</param>
+/// <param name="col">Columna on està la persona</param>
+public void buidar (int fil, int col)
+/// <summary>
+/// Posa una Persona dins de l'escenari i a la taula de persones
+/// Si la posició de la persona ja està ocupada, genera una excepció
+/// </summary>
+/// <param name="pers">Persona a afegir</param>
+public void posar (Persona pers)
+/// <summary>
+/// Mira si en el tauler hi ha alguna persona amb aquest nom
+/// </summary>
+/// <param name="nom">Nom a cercar</param>
+/// <returns>Si hi ha coincidència</returns>
+public bool NomRepetit (string nom)
+/// <summary>
+/// Fa que totes les persones facin un moviment
+/// </summary>
+public void Cicle ()
+}
---
+++ ReunioSocial/ClassesParty/Persona.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesParty
+{
+    public abstract class Persona : Posicio
+    {
+
+        string nom;
+
+        /// <summary>
+        /// Crea una persona
+        /// </summary>
+        /// <param name="nom">Strng que identifica la persona</param>
+        /// <param name="fil">Fila on està localitzada</param>
+        /// <param name="col">Columna on està localitzada</param>
+        public Persona(string nom, int fil, int col) : base(fil, col)
+        {
+            this.nom = nom;
+        }
+        /// <summary>
+        /// Crea una persona
+        /// </summary>
+        /// <param name="nom">nom de la persona</param>
+        public Persona(string nom)
+        {
+            this.nom = nom;
+            this.Fila = 0;
+            this.Columna = 0;
+        }
+        /// <summary>
+        /// Crea una persona
+        /// </summary>
+        public Persona() : base()
+        {
+            this.nom = "Anonymous";
+            this.Fila = 0;
+            this.Columna = 0;
+        }
+        /// <summary>
+        /// Obté el nom de la persona
+        /// </summary>
+        public string Nom
+        {
+            get { return nom;}
+        }
+        /// <summary>
+        /// Retorna que la posició ocupada per aquesta persona no està buida
+        /// </summary>
+        public override bool Buida
+        {
+            get { return false;}
+        }
+        /// <summary>
+        /// Atraccio de persona sobre una determinada posicio
+        /// </summary>
+        /// <param name="fil">Fila de la posició</param>
+        /// <param name="col">Columan de la posició</param>
+        /// <param name="esc">Escenari on estan situats</param>
+        /// <returns>Atracció quantificada</returns>
+        private double Atraccio(int fil, int col, Escenari esc)
+        { 
+            
+        }
+        /// <summary>
+        /// Decideix quin serà el següent moviment que farà la persona
+        /// </summary>
+        /// <param name="esc">Escenari on esta situada la persona</param>
+        /// <returns>Una de les 5 possibles direccions (Quiet, Amunt, Avall, Dreta, Esquerra</returns>
+        public Direccio OnVaig(Escenari esc)
+        {
+            double atraccio = Atraccio(this.Fila, this.Columna, esc);
+            
+            return Direccio.Dreta;
+        }
+        /// <summary>
+        /// Interès de la persona sobre una determinada posició
+        /// </summary>
+        /// <param name="pos">Posició</param>
+        /// <returns>Interès quantificat</returns>
+        public abstract int Interes(Posicio pos);
+        /// <summary>
+        /// Determina si la persona es un convidat (home o dona) o un cambrer
+        /// </summary>
+        /// <returns>Retorna si és convidat</returns>
+        public abstract bool EsConvidat();
+    }
+}
