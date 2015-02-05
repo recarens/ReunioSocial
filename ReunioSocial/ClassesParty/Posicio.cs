781f55c2cea32c7d5389e98de27ed1fdaa33e52b
recarens <eric.recarens@gmail.com>
2015-02-05 17:25:08 +0100

    Restructuracio
    primera restructuracio

--- ReunioSocial/ClassesParty/Posicio.cs
+++ ReunioSocial/ClassesParty/Posicio.cs
@@ -9,32 +9,65 @@ namespace ClassesParty
     public enum Direccio { Quiet, Amunt, Dreta, Avall, Esquerra }
     public class Posicio
     {
-        int columna;
         int fila;
+        int columna;
 
+        /// <summary>
+        /// Crea una nova posició
+        /// </summary>
+        /// <param name="fil">Fila de la Posició</param>
+        /// <param name="col">Columna de la Posició</param>
+        public Posicio(int fil, int col)
+        {
+            this.fila = fil;
+            this.columna = col;
+        }
+        /// <summary>
+        /// Crea una nova posició amb fila i columna igual a 0
+        /// </summary>
+        public Posicio()
+        {
+            this.fila = 0;
+            this.columna = 0;
+        }
+        /// <summary>
+        /// Assigna o obté la columna de la posicio
+        /// </summary>
         public int Columna
         {
             get { return columna; }
             set { columna = value; }
         }
+        /// <summary>
+        /// Assigna o obté la fila de la posicio
+        /// </summary>
         public int Fila
         {
             get { return fila; }
             set { fila = value; }
         }
-        public bool Buida
-        {
-            get { return true; }
-        }
 
-        double Distancia(Posicio pos)
+        /// <summary>
+        /// Retorna si la posició està o no buida
+        /// </summary>
+        public virtual bool Buida
+        {
+            get { return true;}
+        }
+        /// <summary>
+        /// Retorna la distància entre dues posicions
+        /// </summary>
+        /// <param name="pos1">Primera posició</param>
+        /// <param name="pos2">Segona posició</param>
+        /// <returns>Distància entre les dues posicions</returns>
+        public static double Distancia(Posicio pos1, Posicio pos2)
         {
             double distancia = 0;
-            int numColumnes = Math.Abs(this.Fila - pos.Fila);
-            int numFiles =  Math.Abs(this.Columna - pos.Columna);
+            int numColumnes = Math.Abs(pos1.Fila - pos2.Fila);
+            int numFiles = Math.Abs(pos1.Columna - pos2.Columna);
             distancia = Math.Sqrt((numColumnes * numColumnes) + (numColumnes * numColumnes));
             return distancia;
         }
-        
+
     }
 }
