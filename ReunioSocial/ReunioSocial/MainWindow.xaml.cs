c3b2f546b1ebe38d8cb1acea6cfd87cac04710b0
David Castello <davidcastello91@gmail.com>
2015-02-19 00:28:08 +0100

    Inicialització esceneri modularitzat i codi netejat
    Segueix faltant la imatge del convidat

--- ReunioSocial/ReunioSocial/MainWindow.xaml.cs
+++ ReunioSocial/ReunioSocial/MainWindow.xaml.cs
@@ -53,46 +53,8 @@ namespace ReunioSocial
         /// </summary>
         private void PintarEscenari()
         {
-
-        }
-
-        private void CrearPersones()
-        {
-            
-        }
-
-        private void btnConfigura_Click(object sender, RoutedEventArgs e)
-        {
-            Window configura = new ConfigEscenari(this);
-            configura.ShowDialog();
-        }
-
-        /// <summary>
-        /// crida els metodes inicia Escenari i Graella
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void btnInicia_Click(object sender, RoutedEventArgs e)
-        {
-            iniciaEscenari();
-            iniciaGraella();
-   
-        }
-
-
-        /// <summary>
-        /// inicia un escenari amb files, columnes, homes dones etc...
-        /// </summary>
-        private void iniciaEscenari()
-        {
-            esc = new Escenari(num_files, num_columnes);
-            grdEscenari.ColumnDefinitions.Clear();
-            grdEscenari.RowDefinitions.Clear();
-            grdEscenari.Children.Clear();
-
             ColumnDefinition colDef;
             RowDefinition rowDef;
-            TextBlock persona;
 
             // columnes grid Escenari
             for (int i = 0; i < num_columnes; i++)
@@ -100,27 +62,34 @@ namespace ReunioSocial
                 colDef = new ColumnDefinition();
                 grdEscenari.ColumnDefinitions.Add(colDef);
             }
+
             // files grid Escenari
             for (int i = 0; i < num_files; i++)
             {
                 rowDef = new RowDefinition();
                 grdEscenari.RowDefinitions.Add(rowDef);
             }
+        }
 
+        private void CrearPersones()
+        {
             Random r = new Random();
-            int nomRandom, sexe, fila, columna, simpatia;
+            int nomRandom, sexe, fila, columna;
+
+            TextBlock persona;
+
 
             // Generem les dones
-            for(int dona = 0; dona < num_dones ; dona++)
+            for (int dona = 0; dona < num_dones; dona++)
             {
                 // generem valors aleatòris
-                fila = r.Next(0, num_files-1);
-                columna = r.Next(0, num_columnes-1);
+                fila = r.Next(0, num_files - 1);
+                columna = r.Next(0, num_columnes - 1);
                 //nomRandom = r.Next(0,nomsDones.Count());
-                sexe = r.Next(0,4);
-                
+                sexe = r.Next(0, 4);
+
                 // Creem una nova dona i la col·loquem a l'escenari
-                Dona d = new Dona(nomsDones[dona],sexe);
+                Dona d = new Dona(nomsDones[dona], sexe);
                 d.Columna = columna;
                 d.Fila = fila;
                 esc.posar(d);
@@ -180,8 +149,55 @@ namespace ReunioSocial
                 Grid.SetRow(persona, c.Fila);
                 grdEscenari.Children.Add(persona);
             }
+        }
 
-            // Assignem les simpaties
+        private void btnConfigura_Click(object sender, RoutedEventArgs e)
+        {
+            Window configura = new ConfigEscenari(this);
+            configura.ShowDialog();
+        }
+
+        /// <summary>
+        /// crida els metodes inicia Escenari i Graella
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnInicia_Click(object sender, RoutedEventArgs e)
+        {
+            iniciaEscenari();
+            iniciaGraella();
+        }
+
+
+        /// <summary>
+        /// inicia un escenari amb files, columnes, homes dones etc...
+        /// </summary>
+        private void iniciaEscenari()
+        {
+            esc = new Escenari(num_files, num_columnes);
+            grdEscenari.ColumnDefinitions.Clear();
+            grdEscenari.RowDefinitions.Clear();
+            grdEscenari.Children.Clear();
+
+            // Generem l'escenari
+            PintarEscenari();
+
+            // Generem totes les persones de l'escenari
+            CrearPersones();
+
+            // Assignem les simpaties a tots els convidats anvers tots els convidats menys a ell mateix i als cambrers
+            assignarSimpaties();
+            
+        }
+
+        /// <summary>
+        /// Assignem les simpaties de tots els convidats
+        /// </summary>
+        private void assignarSimpaties()
+        {
+            Random r = new Random();
+            int simpatia;
+
             for (int person = 0; person < esc.Tp.NumPersones; person++)
             {
                 for (int personaALaLlista = 0; personaALaLlista < esc.Tp.NumPersones; personaALaLlista++)
@@ -204,7 +220,7 @@ namespace ReunioSocial
                                 d[esc.Tp.ElementAt(personaALaLlista).Nom] = simpatia;
                             }
                         }
-                        
+
                     }
                 }
             }
@@ -212,20 +228,76 @@ namespace ReunioSocial
 
         private void iniciaGraella()
         {
+            // Reiniciem la graella
             grdGraella.ColumnDefinitions.Clear();
             grdGraella.RowDefinitions.Clear();
             grdGraella.Children.Clear();
 
+            // Calculem el nombre de convidats per fer la graella sense tenir en compte els cambrers
+            // ja que no cal que apareixin
             num_convidats = num_dones + num_homes;
 
+            inicialitzaGraella();
+
+            ompleSimpaties();
+
+            grdEscenari.ShowGridLines = true;
+            grdGraella.ShowGridLines = true;
+            
+        }
+
+        #region GENEREM TOTA LA GRAELLA DE SIMPATIES
+        private void ompleSimpaties()
+        {
+            TextBlock simpatiaPersona;
+
+            // Mostrem les simpaties de cada convidat anvers a totes les altres a la graella
+            for (int i = 0; i < num_convidats; i++)
+            {
+                for (int j = 0; j < num_convidats; j++)
+                {
+                    if (j != i && !(esc.Tp.ElementAt(i) is Cambrer) && !(esc.Tp.ElementAt(j) is Cambrer))
+                    {
+                        simpatiaPersona = new TextBlock();
+                        simpatiaPersona.FontSize = 16;
+                        simpatiaPersona.FontWeight = FontWeights.Bold;
+                        simpatiaPersona.VerticalAlignment = VerticalAlignment.Center;
+                        simpatiaPersona.HorizontalAlignment = HorizontalAlignment.Center;
+
+                        if (esc.Tp.ElementAt(i) is Home)
+                        {
+                            Home aux = (Home)esc.Tp.ElementAt(i);
+                            simpatiaPersona.Text = aux[esc.Tp.ElementAt(j).Nom].ToString();
+                        }
+                        else if (esc.Tp.ElementAt(i) is Dona)
+                        {
+                            Dona aux = (Dona)esc.Tp.ElementAt(i);
+                            simpatiaPersona.Text = aux[esc.Tp.ElementAt(j).Nom].ToString();
+                        }
+
+                        Grid.SetColumn(simpatiaPersona, j + 1);
+                        Grid.SetRow(simpatiaPersona, i + 1);
+                        grdGraella.Children.Add(simpatiaPersona);
+                    }
+                }
+            }
+
+            
+        }
+
+        // Inicialitzem la graella amb els noms dels convidats a la primera fila i també a la primera columna
+        private void inicialitzaGraella()
+        {
             ColumnDefinition colDef;
             RowDefinition rowDef;
-            TextBlock nomPersona, simpatiaPersona;
+            TextBlock nomPersona;
 
             ////Columna de noms
             colDef = new ColumnDefinition();
             grdGraella.ColumnDefinitions.Add(colDef);
-            //columnes grid Graella
+
+            // Generem les columnes de la graella 
+            // i també posem els noms dels convidats a la primera fila
             for (int i = 0; i < num_convidats; i++)
             {
                 colDef = new ColumnDefinition();
@@ -241,14 +313,16 @@ namespace ReunioSocial
                 nomPersona.VerticalAlignment = VerticalAlignment.Top;
                 nomPersona.HorizontalAlignment = HorizontalAlignment.Center;
                 Grid.SetRow(nomPersona, 0);
-                Grid.SetColumn(nomPersona, i+1);
+                Grid.SetColumn(nomPersona, i + 1);
                 grdGraella.Children.Add(nomPersona);
             }
 
             //Fila de noms
             rowDef = new RowDefinition();
             grdGraella.RowDefinitions.Add(rowDef);
-            //files grid Graella
+
+            // Generem les files de la graella 
+            // i també posem els noms dels convidats a la primera columna
             for (int i = 0; i < num_convidats; i++)
             {
                 rowDef = new RowDefinition();
@@ -261,45 +335,13 @@ namespace ReunioSocial
                 nomPersona.Foreground = new SolidColorBrush(Colors.Black);
                 nomPersona.VerticalAlignment = VerticalAlignment.Center;
                 nomPersona.HorizontalAlignment = HorizontalAlignment.Center;
-                Grid.SetRow(nomPersona, i+1);
+                Grid.SetRow(nomPersona, i + 1);
                 Grid.SetColumn(nomPersona, 0);
                 grdGraella.Children.Add(nomPersona);
             }
-
-            // Mostrem les simpaties
-            for (int i = 0; i < num_convidats; i++)
-            {
-                for (int j = 0; j < num_convidats; j++)
-                {
-                    if(j != i && !(esc.Tp.ElementAt(i) is Cambrer) && !(esc.Tp.ElementAt(j) is Cambrer))
-                    {
-                        simpatiaPersona = new TextBlock();
-                        simpatiaPersona.FontSize = 16;
-                        simpatiaPersona.FontWeight = FontWeights.Bold;
-                        simpatiaPersona.VerticalAlignment = VerticalAlignment.Center;
-                        simpatiaPersona.HorizontalAlignment = HorizontalAlignment.Center;
-
-                        if (esc.Tp.ElementAt(i) is Home)
-                        {
-                            Home aux = (Home)esc.Tp.ElementAt(i);
-                            simpatiaPersona.Text = aux[esc.Tp.ElementAt(j).Nom].ToString();
-                        }
-                        else if(esc.Tp.ElementAt(i) is Dona)
-                        {
-                            Dona aux = (Dona)esc.Tp.ElementAt(i);
-                            simpatiaPersona.Text = aux[esc.Tp.ElementAt(j).Nom].ToString();
-                        }
-
-                        Grid.SetColumn(simpatiaPersona, j + 1);
-                        Grid.SetRow(simpatiaPersona, i + 1);
-                        grdGraella.Children.Add(simpatiaPersona);
-                    }
-                }
-            }
-
-                grdEscenari.ShowGridLines = true;
-            grdGraella.ShowGridLines = true;
         }
+
+        #endregion
 
 
         #region PROPIETATS
